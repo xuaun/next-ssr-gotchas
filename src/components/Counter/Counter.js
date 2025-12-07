9e2e8198e7de658446d1ead206bb668d3c309a92
João Víctor de Araujo Lima <joaovictor.ne@gmail.com>
2025-12-07 13:54:35 -0300

    default value - Sppiner

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,23 +1,26 @@
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
+
+import Spinner from "../Spinner";
 
 function Counter() {
-  const [count, setCount] = React.useState(() => {
-    return Number(
-      window.localStorage.getItem('saved-count') || 0
-    );
-  });
+  const [count, setCount] = React.useState(null);
 
   React.useEffect(() => {
-    window.localStorage.setItem('saved-count', count);
+    const savedValue = window.localStorage.getItem("saved-count");
+
+    setCount(savedValue ? Number(savedValue) : 0);
+  }, []);
+
+  React.useEffect(() => {
+    if (typeof count === "number") {
+      window.localStorage.setItem("saved-count", count);
+    }
   }, [count]);
 
   return (
-    <button
-      className="count-btn"
-      onClick={() => setCount(count + 1)}
-    >
-      Count: {count}
+    <button className="count-btn" onClick={() => setCount(count + 1)}>
+      Count: {typeof count === "number" ? count : <Spinner />}
     </button>
   );
 }
